e21919fe5b99039ab6fb20f67613882bffeff4c5
Josh Day <emailjoshday@gmail.com>
2017-03-16 09:46:44 -0400

    first commit

--- src/APIGenerator.jl
+++ src/APIGenerator.jl
@@ -1,5 +1,22 @@
 module APIGenerator
 
-# package code goes here
+function make_api(pkg::String, dest::String; title="$pkg API")
+    if dest[end-3:end] != ".md"
+        dest *= ".md"
+    end
+    Pkg = eval(parse("Main.$pkg"))
+    touch(dest)
+    file = open(dest, "r+")
+    write(file, "<!--- Generated at " * string(now()) * ".  Don't edit --->\n")
+    write(file, "# $title\n\n")
+    info("The following items are included in the output file:\n")
+    nms = names(Pkg)
+    for nm in nms
+        d = @doc @eval Pkg.$nm
+        println("  > $nm")
+        write(file, "## $(string(nm)) \n $(Markdown.plain(d)) \n")
+    end
+    close(file)
+end
 
 end # module
