1a771c94a1ca6588d7670abb8564518d91265e28
Josh Day <emailjoshday@gmail.com>
2017-03-16 10:06:08 -0400

    working now

--- src/APIGenerator.jl
+++ src/APIGenerator.jl
@@ -1,18 +1,25 @@
 module APIGenerator
 
-function make_api(pkg::String, dest::String; title="$pkg API")
-    if dest[end-3:end] != ".md"
+function make_api(pkg::String, dest::String; title="$pkg API", readme=false, clobber=true)
+    if dest[end-2:end] != ".md"
         dest *= ".md"
     end
-    Pkg = eval(parse("Main.$pkg"))
+    Pkg = Symbol(pkg)
+    clobber && isfile(dest) && rm(dest)
     touch(dest)
     file = open(dest, "r+")
-    write(file, "<!--- Generated at " * string(now()) * ".  Don't edit --->\n")
+    write(file, "<!--- Generated at " * string(now()) * ".  Don't edit --->\n\n")
     write(file, "# $title\n\n")
     info("The following items are included in the output file:\n")
-    nms = names(Pkg)
+    nms = names(@eval(Main.$Pkg))
+    if readme
+        info("Writing $pkg README")
+        write(file, Markdown.plain(Docs.doc(@eval(Main.$Pkg))))
+    end
+    nms = setdiff(nms, [:OnlineStats])
     for nm in nms
-        d = @doc @eval Pkg.$nm
+        @eval obj = Main.$Pkg.$nm
+        d = Docs.doc(obj)
         println("  > $nm")
         write(file, "## $(string(nm)) \n $(Markdown.plain(d)) \n")
     end
